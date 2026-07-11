6e04457e3c8aa75b70bd795867ded8866d45912a
kafkaesquez <archer8bitt@gmail.com>
2026-07-11 10:35:23 +0800

    working on ladder script (WIP)

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -7,15 +7,20 @@ public class Ladder : MonoBehaviour
     private float vertical;
     [SerializeField] private float Speed; //so that its easier to edit on inspector depending on the movement script that jeremy comes up with.
     [SerializeField] private float gravity; //reference to jeremy's gravity ltr on
+    [SerializeField] private Collider2D FinalHitbox;  //to trigger the animations for mario finishing his ladder climb, and to differentiate the two trigger hitboxes
+
     private bool isLadder;
     private bool isClimbing;
+    private bool isClimbingFinish;
+
+    public Animator marioanim;
 
     [SerializeField] private Rigidbody2D rb; //so we can reference mario's hitbox
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        marioanim = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -23,8 +28,13 @@ public class Ladder : MonoBehaviour
         if (collision.CompareTag("Ladder"))
         {
             isLadder = true;
+            print("im in the ladder");
         }
-            
+        if (collision == FinalHitbox)
+        {
+            isClimbingFinish = true; 
+        }
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -33,6 +43,7 @@ public class Ladder : MonoBehaviour
         {
             isLadder = false;
             isClimbing = false;
+            isClimbingFinish = false;
         }
     }
 
@@ -41,7 +52,7 @@ public class Ladder : MonoBehaviour
     {
         vertical = Input.GetAxis("Vertical");
 
-        if (isLadder && Mathf.Abs(vertical) < 0)
+        if (isLadder && Mathf.Abs(vertical) > 0)
         {
             isClimbing = true;
         }
@@ -51,8 +62,14 @@ public class Ladder : MonoBehaviour
     {
         if (isClimbing)
         {
+            print("im gonna climbnow");
             rb.gravityScale = 0f;
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, vertical * Speed); //makes it so mario can go up and down without gravity weighing on him
+            marioanim.SetBool("isClimbing", isClimbing);
+            if (isClimbingFinish)
+            {
+                marioanim.SetTrigger("isClimbingFinish");
+            }
         }
         else
         {
