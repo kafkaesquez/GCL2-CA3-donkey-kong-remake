9f0092af15d401ac51cb8802e742ca39aa15d767
kafkaesquez <archer8bitt@gmail.com>
2026-07-22 00:05:34 +0800

    laddering ladders
    made it so theres a ladderitem which mario can pickup to now build his own ladder... awesome sauce..

--- Assets/Scripts/LadderItem.cs
+++ Assets/Scripts/LadderItem.cs
@@ -5,6 +5,10 @@ public class LadderItem : MonoBehaviour
 
     public Animator marioanim;
     public bool holdingLadder;
+    [SerializeField] private GameObject LadderItemObject;
+    [SerializeField] private GameObject ladderPrefab; // to reference which object to "build"
+    [SerializeField] private Transform placePoint; // empty object infront of mario to reference where to place the ladder
+    [SerializeField] private GameObject LadderInstructions; //to show the player an intuitive reference of what to click to place the ladder down
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,18 +17,33 @@ public class LadderItem : MonoBehaviour
         holdingLadder = false;
     }
 
-    private void OnTriggeEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("LadderItem"))
         {
             holdingLadder |= true;
             marioanim.SetBool("HoldingLadder", holdingLadder);
-            Destroy(collision.gameObject);
+            Destroy(LadderItemObject);
+            LadderInstructions.SetActive(true);
         }
     }
+
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (holdingLadder && Input.GetMouseButtonDown(0))
+        {
+            PlaceLadder(); //left mouse click to build the ladder
+        }
+    }
+
+    void PlaceLadder()
+    {
+        Vector3 spawnPos = placePoint != null ? placePoint.position : transform.position; //null is there as a safeholder just in case placepoint is empty, so it falls to marios own position
+        Instantiate(ladderPrefab, spawnPos, Quaternion.identity); 
+
+        holdingLadder = false;
+        marioanim.SetBool("HoldingLadder", holdingLadder);
     }
 }
