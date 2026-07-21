f7a41e31df4e395de2095a7402383be1f7515538
shin5dabz-oss <shin5dabz@gmail.com>
2026-07-22 01:01:07 +0800

    fixed ending animation

--- Assets/Scripts/WinSequence.cs
+++ Assets/Scripts/WinSequence.cs
@@ -10,6 +10,9 @@ public class WinSequence : MonoBehaviour
     public Animator donkeyKongAnimator;
     public Animator paulineAnimator;
 
+    public Spawner barrelSpawner;
+
+    public Sprite marioIdleSprite;
     public void StartWinSequence()
     {
         StartCoroutine(PlayWinSequence());
@@ -21,14 +24,41 @@ public class WinSequence : MonoBehaviour
         //stop pauline animation
 
         MarioMovement marioMovement = mario.GetComponent<MarioMovement>();
+        Animator marioAnimator = mario.GetComponent<Animator>();
 
+        //stop Mario from moving
         marioMovement.canMove = false;
+
+        //stop whatever animation is currently playing
+        marioAnimator.enabled = false;
+
+        //force the sprite to be the idle sprite
+        SpriteRenderer marioSprite = mario.GetComponent<SpriteRenderer>();
+        marioSprite.sprite = marioIdleSprite;
+
+
 
         Rigidbody2D rb = mario.GetComponent<Rigidbody2D>();
 
         rb.linearVelocity = Vector2.zero;
         rb.angularVelocity = 0f;
         //stops mario from moving
+
+        barrelSpawner.enabled = false; //pause barrel spawner
+
+        GameObject[] barrels = GameObject.FindGameObjectsWithTag("Barrel");
+
+        foreach (GameObject barrel in barrels)
+        {
+            Rigidbody2D barrelrb = barrel.GetComponent<Rigidbody2D>();
+
+            if (barrelrb != null)
+            {
+                barrelrb.linearVelocity = Vector2.zero;
+                barrelrb.angularVelocity = 0f;
+                barrelrb.simulated = false; //freezes all physics 
+            }
+        }
 
         yield return new WaitForSeconds(1f);
         //pause 1 sec
@@ -42,8 +72,8 @@ public class WinSequence : MonoBehaviour
         donkeyKongAnimator.SetTrigger("Shock");
         //play donkey kong shocked animation 
 
-        yield return new WaitForSeconds(4f);
-        //pause 2 secs
+        yield return new WaitForSeconds(5f);
+        //pause 5 secs
 
         SceneManager.LoadScene("WinScene");
     }
